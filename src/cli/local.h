d55967f4cc52107fefd2db632d66508d3d341a8b
Mohammad-Mualla-CE <103336547+Mohammad-Mualla-CE@users.noreply.github.com>
2023-01-04 09:23:55 +0200

    Update local.h file

--- src/cli/local.h
+++ src/cli/local.h
@@ -8,46 +8,41 @@
 #include <stdlib.h>
 #include <string.h>
 #include <pthread.h>
-#include "../include.h"
 
-#define SLEEP_MIN 50 * 1000  // 30 ms
-#define SLEEP_MAX 300 * 1000 // 100 ms
+#define g_number_of_manufacturing_lines_to_produce_the_chocolate_products 7
 
-struct personInformation
-{
-        pid_t personID;                      // Process ip
-        char gender;                         // F,M
-        DocumentType officialDocumentNeeded; // "0"birth certificates, "1"travel documents, "2"family reunion documents, "3"ID-related problems
-        int indexLocationInTheHostQueue;
-        int tiketNumberInGroupingArea;
-        Location currentLocation;
-};
+#define g_number_of_manufacturing_lines_to_produce_the_chocolate_typeA 3
+#define g_number_of_manufacturing_lines_to_produce_the_chocolate_typeB 2
+#define g_number_of_manufacturing_lines_to_produce_the_chocolate_typeC 2
 
-struct accessQueueNode
-{
-        struct personInformation personInfo;
-        struct accessQueueNode *nextPesron;
-};
+#define g_number_of_employees_per_manufacturing_line_each_executing_a_step_in_the_chocolate_manufacturing_process_typeA 8
+#define g_number_of_employees_per_manufacturing_line_each_executing_a_step_in_the_chocolate_manufacturing_process_typeB 6
+#define g_number_of_employees_per_manufacturing_line_each_executing_a_step_in_the_chocolate_manufacturing_process_typeC 5
 
-struct parent_child_message_payload
-{
-        int index_in_queue; // index if inside a queue
-        Location current_location;
-};
 
-typedef struct parent_child_message_payload parent_child_message_payload;
+#define g_min_time_for_Each_step_typeA 0 //!?
+#define g_max_time_for_Each_step_typeA 0 //!?
+#define g_min_time_for_Each_step_typeB 0 //!?
+#define g_max_time_for_Each_step_typeB 0 //!?
+#define g_min_time_for_Each_step_typeC 0 //!?
+#define g_max_time_for_Each_step_typeC 0 //!?
 
-struct parent_message_buf
-{
-        long pid; // first field of the message struct should be the message type
-        parent_child_message_payload payload;
-};
+/* 2 employees for all types , or 2 employees for each type //!? */
+#define g_number_of_employees_to_collect_chocolate_in_patches_of_10_pieces_per_type_and_sent_to_printing_the_expiration_date 2
+#define g_number_of_pieces_per_patches 10 //!?
 
-struct PidNode // for addProcessToListThatHaveAllProcessesIDs
-{
-        pid_t personID;
-        struct PidNode *nextPid;
-};
+#define g_time_for_printing_the_expiration_date_per_patches 0 //!?
 
-typedef struct parent_message_buf parent_message_buf;
+#define g_number_of_employess_to_move_from_printing_expiration_date_machine_to_containers_according_to_the_type_of_chocolate 3
+
+#define g_number_of_employess_to_fill_the_carton_boxes_from_the_containers 3
+#define g_number_of_chocolate_products_per_carton 20 //!? chocolate_products == patches
+
+#define g_number_of_storage_employees_work_to_move_the_filled_carton_boxes_and_place_them_in_the_storge_area 2
+
+
+#define g_number_of_loading_employees_to_load_trucks_with_the_merchandise_from_storage_area 2
+
+#define g_number_of_shipping_trucks 3
+
 #endif
