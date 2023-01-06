28f4d93f2d56177ad1e0656786bcb80b6a53987c
Mohammad-Mualla-CE <mohammadmualla111@gmail.com>
2023-01-06 11:19:53 +0200

    new 4 ://test on typeB lines : 2 lines and Steps 1 to 6 have to happen in order "Not ready "

--- src/cli/main.c
+++ src/cli/main.c
@@ -30,6 +30,13 @@ pthread_t g_Array_of_Threads_TypeB[2][6];
 //Array of threads of_employees_per_manufacturing_line_each_executing_a_step_in_the_chocolate_manufacturing_process_typeC
 pthread_t g_Array_of_Threads_TypeC[2][5];
 
+
+//Arrays of mutex //Steps 1 to 6 have to happen in order
+pthread_mutex_t g_mutexs_for_lines_of_typeB[2][6] = {{PTHREAD_MUTEX_INITIALIZER, PTHREAD_MUTEX_INITIALIZER,PTHREAD_MUTEX_INITIALIZER, PTHREAD_MUTEX_INITIALIZER,PTHREAD_MUTEX_INITIALIZER, PTHREAD_MUTEX_INITIALIZER},{PTHREAD_MUTEX_INITIALIZER, PTHREAD_MUTEX_INITIALIZER,PTHREAD_MUTEX_INITIALIZER, PTHREAD_MUTEX_INITIALIZER,PTHREAD_MUTEX_INITIALIZER, PTHREAD_MUTEX_INITIALIZER}};
+int g_count_for_mutex_index_typeB_line1=0;
+int g_count_for_mutex_index_typeB_line2=0;
+
+
 int g_number_of_threads_per_project =0;
 
 
@@ -43,6 +50,7 @@ void createThreadsForEmpsOfTypeB();
 void createThreadsForEmpsOfTypeC();
 int randomIntegerInRange(int lower, int upper);
 void employeeDoStepPerLine(void *time_for_emp_takes_to_finsh_step_per_line);
+void employeeDoStepPerLine_TypeB(void *line_number_as_index);
 
 
 
@@ -67,9 +75,9 @@ void createThreads(){
         TypeB : 2*6 =12
         TypeC : 2*5 =10
 */
-createThreadsForEmpsOfTypeA();
+//createThreadsForEmpsOfTypeA();
 createThreadsForEmpsOfTypeB();
-createThreadsForEmpsOfTypeC();
+//createThreadsForEmpsOfTypeC();
 
 /* number of threads for employess of collect_chocolate_in_patches_of_10_pieces_per_type_and_sent_to_printing_the_expiration_date : 2 */
 // What about printing_the_expiration_date line 8 should be as thread //!?
@@ -96,16 +104,19 @@ void createThreadsForEmpsOfTypeA(){
 }
 
 void createThreadsForEmpsOfTypeB(){
-     int i,j,time_for_emp_takes_to_finsh_step_per_line_B; 
+     int i,j; 
     for (j=0;j<g_number_of_manufacturing_lines_to_produce_the_chocolate_typeB;j++){        
         for(i=0;i<g_number_of_employees_per_manufacturing_line_each_executing_a_step_in_the_chocolate_manufacturing_process_typeB;i++){
-                time_for_emp_takes_to_finsh_step_per_line_B = randomIntegerInRange(g_min_time_for_Each_step_typeB,g_max_time_for_Each_step_typeB);
-                pthread_create(&g_Array_of_Threads_TypeB[j][i], NULL, (void *)employeeDoStepPerLine, (void *)&time_for_emp_takes_to_finsh_step_per_line_B);
-                pthread_join(g_Array_of_Threads_TypeB[j][i], NULL);
+                pthread_create(&g_Array_of_Threads_TypeB[j][i], NULL, (void *)employeeDoStepPerLine_TypeB, (void *)&j);// j as line index
+                //pthread_join(g_Array_of_Threads_TypeB[j][i], NULL);
                 g_number_of_threads_per_project++;
-                printf("\n%d\n",g_number_of_threads_per_project);
+               
         }
+        
     } 
+    for (j=0;j<g_number_of_manufacturing_lines_to_produce_the_chocolate_typeB;j++)        
+        for(i=0;i<g_number_of_employees_per_manufacturing_line_each_executing_a_step_in_the_chocolate_manufacturing_process_typeB;i++)
+                pthread_join(g_Array_of_Threads_TypeB[j][i], NULL);
 
 }
 
@@ -122,12 +133,147 @@ void createThreadsForEmpsOfTypeC(){
     } 
 
 }
-
 void employeeDoStepPerLine(void *time_for_emp_takes_to_finsh_step_per_line){
-        
         int time = *((int *)time_for_emp_takes_to_finsh_step_per_line);
-        //printf("\n%d\n",time);
         sleep(time);
+}
+void employeeDoStepPerLine_TypeB(void *line_number_as_index){//pass line identifaier 
+        
+        //test on typeB lines : 2 lines and Steps 1 to 6 have to happen in order
+        int line_number_index = *((int *)line_number_as_index);
+        int time = randomIntegerInRange(g_min_time_for_Each_step_typeB,g_max_time_for_Each_step_typeB);
+        while (1)
+        {
+                if (line_number_index==0){
+                        if(g_count_for_mutex_index_typeB_line1==0){//Emp 1
+                                pthread_mutex_lock(&g_mutexs_for_lines_of_typeB[line_number_index][g_count_for_mutex_index_typeB_line1]);
+                                printf("\nEmp line 1: %d lock\n",g_count_for_mutex_index_typeB_line1+1);
+                                sleep(time);
+                                pthread_mutex_unlock(&g_mutexs_for_lines_of_typeB[line_number_index][g_count_for_mutex_index_typeB_line1]);
+                        }
+                        else if(g_count_for_mutex_index_typeB_line1==1){//Emp 2
+                                pthread_mutex_lock(&g_mutexs_for_lines_of_typeB[line_number_index][g_count_for_mutex_index_typeB_line1-1]);//To check if last mutex : relased
+                                pthread_mutex_unlock(&g_mutexs_for_lines_of_typeB[line_number_index][g_count_for_mutex_index_typeB_line1-1]);
+
+                                pthread_mutex_lock(&g_mutexs_for_lines_of_typeB[line_number_index][g_count_for_mutex_index_typeB_line1]);
+                                printf("\nEmp line 1: %d lock\n",g_count_for_mutex_index_typeB_line1+1);
+                                sleep(time);
+                                pthread_mutex_unlock(&g_mutexs_for_lines_of_typeB[line_number_index][g_count_for_mutex_index_typeB_line1]);
+
+                        } 
+                        else if(g_count_for_mutex_index_typeB_line1==2){//Emp 3
+                                pthread_mutex_lock(&g_mutexs_for_lines_of_typeB[line_number_index][g_count_for_mutex_index_typeB_line1-1]);//To check if last mutex : relased
+                                pthread_mutex_unlock(&g_mutexs_for_lines_of_typeB[line_number_index][g_count_for_mutex_index_typeB_line1-1]);
+
+                                pthread_mutex_lock(&g_mutexs_for_lines_of_typeB[line_number_index][g_count_for_mutex_index_typeB_line1]);
+                                printf("\nEmp line 1: %d lock\n",g_count_for_mutex_index_typeB_line1+1);
+                                sleep(time);
+                                pthread_mutex_unlock(&g_mutexs_for_lines_of_typeB[line_number_index][g_count_for_mutex_index_typeB_line1]);
+
+                        }
+                        else if(g_count_for_mutex_index_typeB_line1==3){//Emp 4
+                                pthread_mutex_lock(&g_mutexs_for_lines_of_typeB[line_number_index][g_count_for_mutex_index_typeB_line1-1]);//To check if last mutex : relased
+                                pthread_mutex_unlock(&g_mutexs_for_lines_of_typeB[line_number_index][g_count_for_mutex_index_typeB_line1-1]);
+
+                                pthread_mutex_lock(&g_mutexs_for_lines_of_typeB[line_number_index][g_count_for_mutex_index_typeB_line1]);
+                                printf("\nEmp line 1: %d lock\n",g_count_for_mutex_index_typeB_line1+1);
+                                sleep(time);
+                                pthread_mutex_unlock(&g_mutexs_for_lines_of_typeB[line_number_index][g_count_for_mutex_index_typeB_line1]);
+
+                        }
+                        else if(g_count_for_mutex_index_typeB_line1==4){//Emp 5
+                                pthread_mutex_lock(&g_mutexs_for_lines_of_typeB[line_number_index][g_count_for_mutex_index_typeB_line1-1]);//To check if last mutex : relased
+                                pthread_mutex_unlock(&g_mutexs_for_lines_of_typeB[line_number_index][g_count_for_mutex_index_typeB_line1-1]);
+
+                                pthread_mutex_lock(&g_mutexs_for_lines_of_typeB[line_number_index][g_count_for_mutex_index_typeB_line1]);
+                                printf("\nEmp line 1: %d lock\n",g_count_for_mutex_index_typeB_line1+1);
+                                sleep(time);
+                                pthread_mutex_unlock(&g_mutexs_for_lines_of_typeB[line_number_index][g_count_for_mutex_index_typeB_line1]);
+
+                        }
+                        else if(g_count_for_mutex_index_typeB_line1==5){//Emp 6
+                                pthread_mutex_lock(&g_mutexs_for_lines_of_typeB[line_number_index][g_count_for_mutex_index_typeB_line1-1]);//To check if last mutex : relased
+                                pthread_mutex_unlock(&g_mutexs_for_lines_of_typeB[line_number_index][g_count_for_mutex_index_typeB_line1-1]);
+
+                                pthread_mutex_lock(&g_mutexs_for_lines_of_typeB[line_number_index][g_count_for_mutex_index_typeB_line1]);
+                                printf("\nEmp line 1: %d lock\n",g_count_for_mutex_index_typeB_line1+1);
+                                sleep(time);
+                                pthread_mutex_unlock(&g_mutexs_for_lines_of_typeB[line_number_index][g_count_for_mutex_index_typeB_line1]);
+
+                        }
+
+                        g_count_for_mutex_index_typeB_line1++;
+                        if(g_count_for_mutex_index_typeB_line1 == 6)   
+                                g_count_for_mutex_index_typeB_line1=0; 
+                        
+                }
+                else{
+                        if(g_count_for_mutex_index_typeB_line2==0){//Emp 1
+                                pthread_mutex_lock(&g_mutexs_for_lines_of_typeB[line_number_index][g_count_for_mutex_index_typeB_line2]);
+                                printf("\nEmp line 2: %d lock\n",g_count_for_mutex_index_typeB_line2+1);
+                                sleep(time);
+                                pthread_mutex_unlock(&g_mutexs_for_lines_of_typeB[line_number_index][g_count_for_mutex_index_typeB_line2]);
+                        }
+                        else if(g_count_for_mutex_index_typeB_line2==1){//Emp 2
+                                pthread_mutex_lock(&g_mutexs_for_lines_of_typeB[line_number_index][g_count_for_mutex_index_typeB_line2-1]);//To check if last mutex : relased
+                                pthread_mutex_unlock(&g_mutexs_for_lines_of_typeB[line_number_index][g_count_for_mutex_index_typeB_line2-1]);
+
+                                pthread_mutex_lock(&g_mutexs_for_lines_of_typeB[line_number_index][g_count_for_mutex_index_typeB_line2]);
+                                printf("\nEmp line 2: %d lock\n",g_count_for_mutex_index_typeB_line2+1);
+                                sleep(time);
+                                pthread_mutex_unlock(&g_mutexs_for_lines_of_typeB[line_number_index][g_count_for_mutex_index_typeB_line2]);
+
+                        } 
+                        else if(g_count_for_mutex_index_typeB_line2==2){//Emp 3
+                                pthread_mutex_lock(&g_mutexs_for_lines_of_typeB[line_number_index][g_count_for_mutex_index_typeB_line2-1]);//To check if last mutex : relased
+                                pthread_mutex_unlock(&g_mutexs_for_lines_of_typeB[line_number_index][g_count_for_mutex_index_typeB_line2-1]);
+
+                                pthread_mutex_lock(&g_mutexs_for_lines_of_typeB[line_number_index][g_count_for_mutex_index_typeB_line2]);
+                                printf("\nEmp line 2: %d lock\n",g_count_for_mutex_index_typeB_line2+1);
+                                sleep(time);
+                                pthread_mutex_unlock(&g_mutexs_for_lines_of_typeB[line_number_index][g_count_for_mutex_index_typeB_line2]);
+
+                        }
+                        else if(g_count_for_mutex_index_typeB_line2==3){//Emp 4
+                                pthread_mutex_lock(&g_mutexs_for_lines_of_typeB[line_number_index][g_count_for_mutex_index_typeB_line2-1]);//To check if last mutex : relased
+                                pthread_mutex_unlock(&g_mutexs_for_lines_of_typeB[line_number_index][g_count_for_mutex_index_typeB_line2-1]);
+
+                                pthread_mutex_lock(&g_mutexs_for_lines_of_typeB[line_number_index][g_count_for_mutex_index_typeB_line2]);
+                                printf("\nEmp line 2: %d lock\n",g_count_for_mutex_index_typeB_line2+1);
+                                sleep(time);
+                                pthread_mutex_unlock(&g_mutexs_for_lines_of_typeB[line_number_index][g_count_for_mutex_index_typeB_line2]);
+
+                        }
+                        else if(g_count_for_mutex_index_typeB_line2==4){//Emp 5
+                                pthread_mutex_lock(&g_mutexs_for_lines_of_typeB[line_number_index][g_count_for_mutex_index_typeB_line2-1]);//To check if last mutex : relased
+                                pthread_mutex_unlock(&g_mutexs_for_lines_of_typeB[line_number_index][g_count_for_mutex_index_typeB_line2-1]);
+
+                                pthread_mutex_lock(&g_mutexs_for_lines_of_typeB[line_number_index][g_count_for_mutex_index_typeB_line2]);
+                                printf("\nEmp line 2: %d lock\n",g_count_for_mutex_index_typeB_line2+1);
+                                sleep(time);
+                                pthread_mutex_unlock(&g_mutexs_for_lines_of_typeB[line_number_index][g_count_for_mutex_index_typeB_line2]);
+
+                        }
+                        else if(g_count_for_mutex_index_typeB_line2==5){//Emp 6
+                                pthread_mutex_lock(&g_mutexs_for_lines_of_typeB[line_number_index][g_count_for_mutex_index_typeB_line2-1]);//To check if last mutex : relased
+                                pthread_mutex_unlock(&g_mutexs_for_lines_of_typeB[line_number_index][g_count_for_mutex_index_typeB_line2-1]);
+
+                                pthread_mutex_lock(&g_mutexs_for_lines_of_typeB[line_number_index][g_count_for_mutex_index_typeB_line2]);
+                                printf("\nEmp line 2: %d lock\n",g_count_for_mutex_index_typeB_line2+1);
+                                sleep(time);
+                                pthread_mutex_unlock(&g_mutexs_for_lines_of_typeB[line_number_index][g_count_for_mutex_index_typeB_line2]);
+
+                        }
+
+                        g_count_for_mutex_index_typeB_line2++;
+                        if(g_count_for_mutex_index_typeB_line2 == 6)   
+                                g_count_for_mutex_index_typeB_line2=0;    
+                        
+                } 
+                
+                time = randomIntegerInRange(g_min_time_for_Each_step_typeB,g_max_time_for_Each_step_typeB); 
+
+        }
 }
 
 void readInputFile(){
